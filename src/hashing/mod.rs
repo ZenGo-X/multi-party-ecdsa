b6885b01a95cba53bee217aba3025470f45f6038
Gary Benattar <Gary@MacBook-Pro-de-julie.local>
2018-06-30 21:49:53 +0300

    Adding hashing utility

--- src/hashing/mod.rs
+++ src/hashing/mod.rs
@@ -14,19 +14,7 @@
     @license GPL-3.0+ <https://github.com/KZen-networks/multi-party-ecdsa/blob/master/LICENSE>
 */
 
-pub mod elliptic;
-pub use elliptic::point::Point as Point;
+extern crate ring;
 
-// TODO: When we will have more than one type of elliptic curve, add as features
-pub use elliptic::curves::secp256_k1::EC as EC;
-pub use elliptic::curves::secp256_k1::SK as SK;
-pub use elliptic::curves::secp256_k1::PK as PK;
-
-pub mod arithmetic;
-// TODO: When we will have more than one type of big num library, add as features
-pub use arithmetic::big_gmp::BigInteger as BigInteger;
-
-pub mod hashing;
-
-pub mod party_1;
-pub mod party_2;
+pub mod traits;
+pub mod hash_sha256;
--- src/hashing/traits.rs
+++ src/hashing/traits.rs
@@ -14,19 +14,8 @@
     @license GPL-3.0+ <https://github.com/KZen-networks/multi-party-ecdsa/blob/master/LICENSE>
 */
 
-pub mod elliptic;
-pub use elliptic::point::Point as Point;
+use ::BigInteger as BigInt;
 
-// TODO: When we will have more than one type of elliptic curve, add as features
-pub use elliptic::curves::secp256_k1::EC as EC;
-pub use elliptic::curves::secp256_k1::SK as SK;
-pub use elliptic::curves::secp256_k1::PK as PK;
-
-pub mod arithmetic;
-// TODO: When we will have more than one type of big num library, add as features
-pub use arithmetic::big_gmp::BigInteger as BigInteger;
-
-pub mod hashing;
-
-pub mod party_1;
-pub mod party_2;
+pub trait Hash {
+    fn create_hash(big_ints: Vec<&BigInt>) -> BigInt;
+}
