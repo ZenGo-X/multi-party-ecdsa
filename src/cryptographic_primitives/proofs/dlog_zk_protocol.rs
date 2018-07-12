7f3b18d04b4110948cf8dbcdd569bc736af1c31d
Gary Benattar <Gary@MacBook-Pro-de-julie.local>
2018-07-12 20:41:28 +0300

    Fix typo in header ECSDA -> ECDSA

--- src/cryptographic_primitives/proofs/dlog_zk_protocol.rs
+++ src/cryptographic_primitives/proofs/dlog_zk_protocol.rs
@@ -1,12 +1,12 @@
 /*
-    Multi-party ECSDA
+    Multi-party ECDSA
 
     Copyright 2018 by Kzen Networks
 
     This file is part of Multi-party ECDSA library
     (https://github.com/KZen-networks/multi-party-ecdsa)
 
-    Multi-party ECSDA is free software: you can redistribute
+    Multi-party ECDSA is free software: you can redistribute
     it and/or modify it under the terms of the GNU General Public
     License as published by the Free Software Foundation, either
     version 3 of the License, or (at your option) any later version.
