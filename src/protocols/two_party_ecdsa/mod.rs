324c481f77fd6d445d796a5a133f263b9e071dbf
omer sh <omer.shlomovits@gmail.com>
2018-07-16 19:12:47 +0300

    full sign + test

--- src/protocols/two_party_ecdsa/mod.rs
+++ src/protocols/two_party_ecdsa/mod.rs
@@ -15,5 +15,5 @@
 */
 
 // Fast Secure Two-Party ECDSA Signing by Yehuda Lindell (https://eprint.iacr.org/2017/552.pdf).
-pub mod lindell_2017_keygen;
-pub mod lindell_2017_sign;
+pub mod lindell_2017;
+
