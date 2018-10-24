7483f3880b2995ecf83db25ecafe860f0df29c59
omershlo <omer.shlomovits@gmail.com>
2018-10-24 19:07:11 +0300

    benches

---
+++ benches/multi_party_ecdsa/gg18/keygen.rs
@@ -0,0 +1,93 @@
+#[macro_use]
+extern crate criterion;
+extern crate cryptography_utils;
+extern crate multi_party_ecdsa;
+
+mod bench {
+    use criterion::Criterion;
+    use cryptography_utils::arithmetic::traits::Samplable;
+    use cryptography_utils::elliptic::curves::traits::*;
+    use cryptography_utils::BigInt;
+    use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2018::party_i::*;
+    pub fn bench_full_keygen_party_one_two(c: &mut Criterion) {
+        c.bench_function("keygen", move |b| {
+            b.iter(|| {
+                let parames = Parameters {
+                    threshold: 1,
+                    share_count: 2,
+                };
+                let party1_keys = Keys::create(0);
+                let party2_keys = Keys::create(1);
+
+                let (to_broadcast_from_party1, blind_1) =
+                    party1_keys.phase1_broadcast_phase3_proof_of_correct_key();
+                let (to_broadcast_from_party2, blind_2) =
+                    party2_keys.phase1_broadcast_phase3_proof_of_correct_key();
+
+                // to_broadcast_from_party1/2 is broadcasted.
+                // then blind_i and y_i are broadcasted.
+                // each party assembles the following vectors:
+                let y_vec = vec![party1_keys.y_i.clone(), party2_keys.y_i.clone()];
+                let blind_vec = vec![blind_1.clone(), blind_2.clone()];
+                let bc1_vec = vec![to_broadcast_from_party1, to_broadcast_from_party2];
+
+                // TODO: make each party verify only proofs of other parties
+                //phase2 (including varifying correct paillier):
+                let (vss_scheme_1, secret_shares_1, index1) = party1_keys
+                    .phase1_verify_com_phase3_verify_correct_key_phase2_distribute(
+                        &parames, &blind_vec, &y_vec, &bc1_vec,
+                    )
+                    .expect("invalid key");
+                let (vss_scheme_2, secret_shares_2, index2) = party2_keys
+                    .phase1_verify_com_phase3_verify_correct_key_phase2_distribute(
+                        &parames, &blind_vec, &y_vec, &bc1_vec,
+                    )
+                    .expect("invalid key");
+
+                // each party assembles her secret share vector:
+                let vss_scheme_for_test = vss_scheme_1.clone();
+                let vss_vec = vec![vss_scheme_1, vss_scheme_2];
+                let party1_ss_vec = vec![
+                    secret_shares_1[index1].clone(),
+                    secret_shares_2[index1].clone(),
+                ];
+                let party2_ss_vec = vec![
+                    secret_shares_1[index2].clone(),
+                    secret_shares_2[index2].clone(),
+                ];
+
+                let (_shared_keys_1, dlog_proof_1) = party1_keys
+                    .phase2_verify_vss_construct_keypair_phase3_pok_dlog(
+                        &parames,
+                        &y_vec,
+                        &party1_ss_vec,
+                        &vss_vec,
+                        &(index1 + 1),
+                    )
+                    .expect("invalid vss");
+                let (_shared_keys_2, dlog_proof_2) = party2_keys
+                    .phase2_verify_vss_construct_keypair_phase3_pok_dlog(
+                        &parames,
+                        &y_vec,
+                        &party2_ss_vec,
+                        &vss_vec,
+                        &(index2 + 1),
+                    )
+                    .expect("invalid vss");
+                ;
+
+                let _pk_vec = vec![dlog_proof_1.pk.clone(), dlog_proof_2.pk.clone()];
+                let dlog_proof_vec = vec![dlog_proof_1, dlog_proof_2];
+
+                //both parties run:
+                Keys::verify_dlog_proofs(&parames, &dlog_proof_vec, &y_vec).expect("bad dlog proof");
+            })
+        });
+    }
+    criterion_group!{
+    name = keygen;
+    config = Criterion::default().sample_size(10);
+    targets =self::bench_full_keygen_party_one_two}
+}
+
+criterion_main!(bench::keygen);
--- benches/two_party_ecdsa/lindell_2017/keygen.rs
+++ benches/two_party_ecdsa/lindell_2017/keygen.rs
@@ -1,95 +1,110 @@
 #[macro_use]
-extern crate bencher;
+extern crate criterion;
 extern crate cryptography_utils;
 extern crate multi_party_ecdsa;
 
 mod bench {
-    use bencher::*;
-    use cryptography_utils::{BigInt};
-    use cryptography_utils::elliptic::curves::traits::*;
-    use multi_party_ecdsa::protocols::two_party_ecdsa::lindell_2017::*;
+    use criterion::Criterion;
     use cryptography_utils::arithmetic::traits::Samplable;
+    use cryptography_utils::elliptic::curves::traits::*;
+    use cryptography_utils::BigInt;
+    use multi_party_ecdsa::protocols::two_party_ecdsa::lindell_2017::*;
 
-    pub fn bench_full_keygen_party_one_two(b: &mut Bencher) {
-        b.iter(|| {
-            let party_one_first_message =
-                party_one::KeyGenFirstMsg::create_commitments_with_fixed_secret_share(ECScalar::from(
-                    &BigInt::sample(253),
-                ));
-            let party_two_first_message = party_two::KeyGenFirstMsg::create_with_fixed_secret_share(
-                ECScalar::from(&BigInt::from(10)),
-            );
-            let party_one_second_message = party_one::KeyGenSecondMsg::verify_and_decommit(
-                &party_one_first_message,
-                &party_two_first_message.d_log_proof,
-            ).expect("failed to verify and decommit");
+    pub fn bench_full_keygen_party_one_two(c: &mut Criterion) {
+        c.bench_function("keygen", move |b| {
+            b.iter(|| {
+                let party_one_first_message =
+                    party_one::KeyGenFirstMsg::create_commitments_with_fixed_secret_share(
+                        ECScalar::from(&BigInt::sample(253)),
+                    );
+                let party_two_first_message =
+                    party_two::KeyGenFirstMsg::create_with_fixed_secret_share(ECScalar::from(
+                        &BigInt::from(10),
+                    ));
+                let party_one_second_message = party_one::KeyGenSecondMsg::verify_and_decommit(
+                    &party_one_first_message,
+                    &party_two_first_message.d_log_proof,
+                )
+                .expect("failed to verify and decommit");
 
-            let _party_two_second_message =
-                party_two::KeyGenSecondMsg::verify_commitments_and_dlog_proof(
-                    &party_one_first_message.pk_commitment,
-                    &party_one_first_message.zk_pok_commitment,
-                    &party_one_second_message.zk_pok_blind_factor,
-                    &party_one_second_message.public_share,
-                    &party_one_second_message.pk_commitment_blind_factor,
-                    &party_one_second_message.d_log_proof,
-                ).expect("failed to verify commitments and DLog proof");
+                let _party_two_second_message =
+                    party_two::KeyGenSecondMsg::verify_commitments_and_dlog_proof(
+                        &party_one_first_message.pk_commitment,
+                        &party_one_first_message.zk_pok_commitment,
+                        &party_one_second_message.zk_pok_blind_factor,
+                        &party_one_second_message.public_share,
+                        &party_one_second_message.pk_commitment_blind_factor,
+                        &party_one_second_message.d_log_proof,
+                    )
+                    .expect("failed to verify commitments and DLog proof");
 
-            // init paillier keypair:
-            let paillier_key_pair = party_one::PaillierKeyPair::generate_keypair_and_encrypted_share(
-                &party_one_first_message,
-            );
+                // init paillier keypair:
+                let paillier_key_pair =
+                    party_one::PaillierKeyPair::generate_keypair_and_encrypted_share(
+                        &party_one_first_message,
+                    );
 
-            let party_two_paillier = party_two::PaillierPublic {
-                ek: paillier_key_pair.ek.clone(),
-                encrypted_secret_share: paillier_key_pair.encrypted_share.clone(),
-            };
+                let party_two_paillier = party_two::PaillierPublic {
+                    ek: paillier_key_pair.ek.clone(),
+                    encrypted_secret_share: paillier_key_pair.encrypted_share.clone(),
+                };
 
-            let correct_key_proof =
-                party_one::PaillierKeyPair::generate_ni_proof_correct_key(&paillier_key_pair);
-            party_two::PaillierPublic::verify_ni_proof_correct_key(
-                correct_key_proof,
-                &party_two_paillier.ek,
-            ).expect("bad paillier key");
-            // zk proof of correct paillier key
+                let correct_key_proof =
+                    party_one::PaillierKeyPair::generate_ni_proof_correct_key(&paillier_key_pair);
+                party_two::PaillierPublic::verify_ni_proof_correct_key(
+                    correct_key_proof,
+                    &party_two_paillier.ek,
+                )
+                .expect("bad paillier key");
+                // zk proof of correct paillier key
 
-            // zk range proof
-            let (encrypted_pairs, challenge, proof) = party_one::PaillierKeyPair::generate_range_proof(
-                &paillier_key_pair,
-                &party_one_first_message,
-            );
-            let _result = party_two::PaillierPublic::verify_range_proof(
-                &party_two_paillier,
-                &challenge,
-                &encrypted_pairs,
-                &proof,
-            ).expect("range proof error");
+                // zk range proof
+                let (encrypted_pairs, challenge, proof) =
+                    party_one::PaillierKeyPair::generate_range_proof(
+                        &paillier_key_pair,
+                        &party_one_first_message,
+                    );
+                let _result = party_two::PaillierPublic::verify_range_proof(
+                    &party_two_paillier,
+                    &challenge,
+                    &encrypted_pairs,
+                    &proof,
+                )
+                .expect("range proof error");
 
-            // pdl proof minus range proof
-            let pdl_chal = party_two_paillier.pdl_challenge(&party_one_first_message.public_share);
+                // pdl proof minus range proof
+                let pdl_chal =
+                    party_two_paillier.pdl_challenge(&party_one_first_message.public_share);
 
-            let pdl_prover = paillier_key_pair.pdl_first_stage(&pdl_chal.c_tag);
+                let pdl_prover = paillier_key_pair.pdl_first_stage(&pdl_chal.c_tag);
 
-            let pdl_decom_party2 = party_two::PaillierPublic::pdl_decommit_c_tag_tag(&pdl_chal);
+                let pdl_decom_party2 = party_two::PaillierPublic::pdl_decommit_c_tag_tag(&pdl_chal);
 
-            let pdl_decom_party1 = party_one::PaillierKeyPair::pdl_second_stage(
-                &pdl_prover,
-                &pdl_chal.c_tag_tag,
-                &party_one_first_message,
-                &pdl_decom_party2.a,
-                &pdl_decom_party2.b,
-                &pdl_decom_party2.blindness,
-            ).expect("pdl error party2");
+                let pdl_decom_party1 = party_one::PaillierKeyPair::pdl_second_stage(
+                    &pdl_prover,
+                    &pdl_chal.c_tag_tag,
+                    &party_one_first_message,
+                    &pdl_decom_party2.a,
+                    &pdl_decom_party2.b,
+                    &pdl_decom_party2.blindness,
+                )
+                .expect("pdl error party2");
 
-            party_two::PaillierPublic::verify_pdl(
-                &pdl_chal,
-                &pdl_decom_party1.blindness,
-                &pdl_decom_party1.q_hat,
-                &pdl_prover.c_hat,
-            ).expect("pdl error party1")
+                party_two::PaillierPublic::verify_pdl(
+                    &pdl_chal,
+                    &pdl_decom_party1.blindness,
+                    &pdl_decom_party1.q_hat,
+                    &pdl_prover.c_hat,
+                )
+                .expect("pdl error party1")
+            })
         });
     }
 
-    benchmark_group!(keygen, self::bench_full_keygen_party_one_two);
+    criterion_group!{
+    name = keygen;
+    config = Criterion::default().sample_size(10);
+    targets =self::bench_full_keygen_party_one_two}
 }
 
-benchmark_main!(bench::keygen);
+criterion_main!(bench::keygen);
