516e6bf8b8bc641b2e3a7251c3e1b3388f4bcea2
vhnat@eruptiq <vhnatyk@gmail.com>
2019-02-22 23:36:06 +0200

    set to 5 samples per bench

--- benches/multi_party_ecdsa/gg18/keygen.rs
+++ benches/multi_party_ecdsa/gg18/keygen.rs
@@ -129,7 +129,7 @@ mod bench {
 
     criterion_group! {
     name = keygen;
-    config = Criterion::default().sample_size(10);
+    config = Criterion::default().sample_size(5);
     targets =
     self::bench_full_keygen_party_one_two,
     self::bench_full_keygen_party_two_three,
