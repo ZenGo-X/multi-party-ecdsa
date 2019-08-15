8401fbcb5f2be8201a06ddd7c870911a86ebb2db
Weiliang Li <to.be.impressive@gmail.com>
2019-08-15 10:27:55 +0900

    Fix key_gen.rs clippy warnings and format README

--- benches/multi_party_ecdsa/gg18/keygen.rs
+++ benches/multi_party_ecdsa/gg18/keygen.rs
@@ -29,23 +29,20 @@ mod bench {
     ) -> (Vec<Keys>, Vec<SharedKeys>, Vec<GE>, GE, VerifiableSS) {
         let parames = Parameters {
             threshold: t,
-            share_count: n.clone(),
+            share_count: n,
         };
-        let party_keys_vec = (0..n.clone())
-            .map(|i| Keys::create(i))
-            .collect::<Vec<Keys>>();
+        let party_keys_vec = (0..n).map(Keys::create).collect::<Vec<Keys>>();
 
         let mut bc1_vec = Vec::new();
         let mut decom_vec = Vec::new();
-        for i in 0..n.clone() {
-            let (bc1, decom1) = party_keys_vec[i].phase1_broadcast_phase3_proof_of_correct_key();
+
+        for key in &party_keys_vec {
+            let (bc1, decom1) = key.phase1_broadcast_phase3_proof_of_correct_key();
             bc1_vec.push(bc1);
             decom_vec.push(decom1);
         }
 
-        let y_vec = (0..n.clone())
-            .map(|i| decom_vec[i].y_i.clone())
-            .collect::<Vec<GE>>();
+        let y_vec = (0..n).map(|i| decom_vec[i].y_i).collect::<Vec<GE>>();
         let mut y_vec_iter = y_vec.iter();
         let head = y_vec_iter.next().unwrap();
         let tail = y_vec_iter;
@@ -53,8 +50,8 @@ mod bench {
         let mut vss_scheme_vec = Vec::new();
         let mut secret_shares_vec = Vec::new();
         let mut index_vec = Vec::new();
-        for i in 0..n.clone() {
-            let (vss_scheme, secret_shares, index) = party_keys_vec[i]
+        for key in &party_keys_vec {
+            let (vss_scheme, secret_shares, index) = key
                 .phase1_verify_com_phase3_verify_correct_key_phase2_distribute(
                     &parames, &decom_vec, &bc1_vec,
                 )
@@ -65,12 +62,12 @@ mod bench {
         }
         let vss_scheme_for_test = vss_scheme_vec.clone();
 
-        let party_shares = (0..n.clone())
+        let party_shares = (0..n)
             .map(|i| {
-                (0..n.clone())
+                (0..n)
                     .map(|j| {
                         let vec_j = &secret_shares_vec[j];
-                        vec_j[i].clone()
+                        vec_j[i]
                     })
                     .collect::<Vec<FE>>()
             })
@@ -78,7 +75,7 @@ mod bench {
 
         let mut shared_keys_vec = Vec::new();
         let mut dlog_proof_vec = Vec::new();
-        for i in 0..n.clone() {
+        for i in 0..n {
             let (shared_keys, dlog_proof) = party_keys_vec[i]
                 .phase2_verify_vss_construct_keypair_phase3_pok_dlog(
                     &parames,
@@ -92,23 +89,17 @@ mod bench {
             dlog_proof_vec.push(dlog_proof);
         }
 
-        let pk_vec = (0..n.clone())
-            .map(|i| dlog_proof_vec[i].pk.clone())
-            .collect::<Vec<GE>>();
+        let pk_vec = (0..n).map(|i| dlog_proof_vec[i].pk).collect::<Vec<GE>>();
 
         //both parties run:
         Keys::verify_dlog_proofs(&parames, &dlog_proof_vec, &y_vec).expect("bad dlog proof");
 
         //test
-        let xi_vec = (0..t.clone() + 1)
-            .map(|i| shared_keys_vec[i].x_i.clone())
-            .collect::<Vec<FE>>();
+        let xi_vec = (0..=t).map(|i| shared_keys_vec[i].x_i).collect::<Vec<FE>>();
         let x = vss_scheme_for_test[0]
             .clone()
-            .reconstruct(&index_vec[0..t.clone() + 1], &xi_vec);
-        let sum_u_i = party_keys_vec
-            .iter()
-            .fold(FE::zero(), |acc, x| acc + &x.u_i);
+            .reconstruct(&index_vec[0..=t], &xi_vec);
+        let sum_u_i = party_keys_vec.iter().fold(FE::zero(), |acc, x| acc + x.u_i);
         assert_eq!(x, sum_u_i);
 
         (
