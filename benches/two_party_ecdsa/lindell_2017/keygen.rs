b2581f02c7f35fd09d3bea90b93787429f21f48c
Omer Shlomovits <omer.shlomovits@gmail.com>
2019-09-13 17:13:40 +0300

    bench update

--- benches/two_party_ecdsa/lindell_2017/keygen.rs
+++ benches/two_party_ecdsa/lindell_2017/keygen.rs
@@ -14,9 +14,9 @@ mod bench {
         c.bench_function("keygen", move |b| {
             b.iter(|| {
                 let (party_one_first_message, comm_witness, ec_key_pair_party1) =
-                    party_one::KeyGenFirstMsg::create_commitments_with_fixed_secret_share(
-                        ECScalar::from(&BigInt::sample(253)),
-                    );
+                    party_one::KeyGenFirstMsg::create_commitments_with_fixed_secret_share(ECScalar::from(
+                        &BigInt::sample(253),
+                    ));
                 let (party_two_first_message, _ec_key_pair_party2) =
                     party_two::KeyGenFirstMsg::create_with_fixed_secret_share(ECScalar::from(
                         &BigInt::from(10),
@@ -25,25 +25,21 @@ mod bench {
                     comm_witness,
                     &party_two_first_message.d_log_proof,
                 )
-                .expect("failed to verify and decommit");
+                    .expect("failed to verify and decommit");
 
                 let _party_two_second_message =
                     party_two::KeyGenSecondMsg::verify_commitments_and_dlog_proof(
                         &party_one_first_message,
                         &party_one_second_message,
                     )
-                    .expect("failed to verify commitments and DLog proof");
+                        .expect("failed to verify commitments and DLog proof");
 
                 // init paillier keypair:
                 let paillier_key_pair =
-                    party_one::PaillierKeyPair::generate_keypair_and_encrypted_share(
-                        &ec_key_pair_party1,
-                    );
+                    party_one::PaillierKeyPair::generate_keypair_and_encrypted_share(&ec_key_pair_party1);
 
-                let party_one_private = party_one::Party1Private::set_private_key(
-                    &ec_key_pair_party1,
-                    &paillier_key_pair,
-                );
+                let party_one_private =
+                    party_one::Party1Private::set_private_key(&ec_key_pair_party1, &paillier_key_pair);
 
                 let party_two_paillier = party_two::PaillierPublic {
                     ek: paillier_key_pair.ek.clone(),
@@ -56,7 +52,7 @@ mod bench {
                     correct_key_proof,
                     &party_two_paillier.ek,
                 )
-                .expect("bad paillier key");
+                    .expect("bad paillier key");
                 // zk proof of correct paillier key
 
                 // zk range proof
@@ -64,18 +60,15 @@ mod bench {
                     &paillier_key_pair,
                     &party_one_private,
                 );
-
-                let _result = party_two::PaillierPublic::verify_range_proof(
-                    &party_two_paillier,
-                    &range_proof,
-                )
-                .expect("range proof error");
+                let _result =
+                    party_two::PaillierPublic::verify_range_proof(&party_two_paillier, &range_proof)
+                        .expect("range proof error");
 
                 // pdl proof minus range proof
-                let (party_two_pdl_first_message, pdl_chal_party2) = party_two_paillier
-                    .pdl_challenge(&party_one_second_message.comm_witness.public_share);
+                let (party_two_pdl_first_message, pdl_chal_party2) =
+                    party_two_paillier.pdl_challenge(&party_one_second_message.comm_witness.public_share);
 
-                let (party_one_pdl_first_message, pdl_decommit_party1) =
+                let (party_one_pdl_first_message, pdl_decommit_party1, alpha) =
                     party_one::PaillierKeyPair::pdl_first_stage(
                         &party_one_private,
                         &party_two_pdl_first_message,
@@ -84,20 +77,20 @@ mod bench {
                 let party_two_pdl_second_message =
                     party_two::PaillierPublic::pdl_decommit_c_tag_tag(&pdl_chal_party2);
                 let party_one_pdl_second_message = party_one::PaillierKeyPair::pdl_second_stage(
-                    &party_one_pdl_first_message,
                     &party_two_pdl_first_message,
                     &party_two_pdl_second_message,
                     party_one_private,
                     pdl_decommit_party1,
+                    alpha,
                 )
-                .expect("pdl error party2");
+                    .expect("pdl error party2");
 
                 party_two::PaillierPublic::verify_pdl(
                     &pdl_chal_party2,
                     &party_one_pdl_first_message,
                     &party_one_pdl_second_message,
                 )
-                .expect("pdl error party1")
+                    .expect("pdl error party1")
             })
         });
     }
