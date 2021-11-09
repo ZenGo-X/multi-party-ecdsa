eaf0b4b4457568cebaae50b31d26134898b80cd9
muji <muji@tmpfs.org>
2021-11-09 13:01:15 +0800

    Restore Zeroize attributes in range_proof.rs.

--- src/utilities/mta/range_proofs.rs
+++ src/utilities/mta/range_proofs.rs
@@ -20,8 +20,11 @@ use zk_paillier::zkproofs::DLogStatement;
 
 use serde::{Deserialize, Serialize};
 use std::borrow::Borrow;
+use zeroize::Zeroize;
 
 /// Represents the first round of the interactive version of the proof
+#[derive(Zeroize)]
+#[zeroize(drop)]
 struct AliceZkpRound1 {
     alpha: BigInt,
     beta: BigInt,
@@ -191,6 +194,8 @@ impl AliceProof {
 }
 
 /// Represents first round of the interactive version of the proof
+#[derive(Zeroize)]
+#[zeroize(drop)]
 struct BobZkpRound1 {
     pub alpha: BigInt,
     pub beta: BigInt,
