418a07abb61b5c36ea78b574300b5f1834960a4d
hcheng826 <lilioo826@gmail.com>
2022-12-13 17:39:03 +0800

    set up http connection btw 2 servers

--- eth/client/src/main.rs
+++ eth/client/src/main.rs
@@ -1,11 +1,34 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use reqwest::header::CONTENT_TYPE;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/", format = "plain", data = "<serialized_tx>")]
+async fn send_tx(serialized_tx: &str) -> &'static str {
+    println!("req from client: {}", serialized_tx);
+    let client = reqwest::Client::new();
+    let res = client
+        .post("http://localhost:8001/sign")
+        .header(CONTENT_TYPE, "text/plain")
+        .body(serialized_tx.to_owned())
+        .send()
+        .await
+        .expect("REASON")
+        .text()
+        .await;
+
+    println!("res from server: {:?}", res.unwrap());
+    "Good"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/send-tx", routes![send_tx])
 }
--- eth/server/src/main.rs
+++ eth/server/src/main.rs
@@ -1,11 +1,20 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/", format = "plain", data = "<serialized_tx>")]
+fn sign(serialized_tx: &str) -> &'static str  {
+    println!("{}", serialized_tx);
+    "Server Good"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/sign", routes![sign])
 }
